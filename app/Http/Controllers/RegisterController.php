2190d2f7e27ccccab26b00229cc6db921be0a916
raphael <uglyman.rsg@gmail.com>
2018-08-02 23:11:16 -0300

    regras iniciais de validação e comportamento de falha

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class RegisterController extends Controller
 {
@@ -19,6 +20,25 @@ class RegisterController extends Controller
     */
     public function save(Request $request)
     {
+        //valida os dados recebidos no formulario
+        $validate = Validator::make($request->all(), [
+            'nome' => 'required',
+            'senha' => 'required',
+            'telefone' => 'required',
+            'email'=> 'required|email',
+            'confirmacao_email'=> 'same:email'
+        ]);
+
+        //caso não passe por alguma regra
+        if ($validate->fails()) {
+            //redireciona para o formulário
+            return redirect('/')
+            //com os dados recebidos
+                ->withInput()
+            //e os erros encontrados
+                ->withErrors($validate);
+        }
+
         print_r($request->all());
     }
 }
